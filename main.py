79d98fb0eeb964b91e774b649ef00d71c51ed494
Robeleczek <robeleczek@gmail.com>
2025-03-22 22:10:26 +0100

    Changes: - implemented the basic functionality with a very slow and stupid approach
    TODO:
    - rewrite the code so it actually gets the job done in reasonable time

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 import os
+import numpy as np
 
 ### OKLAB <-> RGB conversions
 # converted from: https://gist.github.com/earthbound19/e7fe15fdf8ca3ef814750a61bc75b5ce
+
+inputImage = None
+inputImagePath = None
+
+inputPalette = None
+inputPalettePath = None
+
+paletteData = []
+
 
 def gammaToLinear(c):
     if c > 0.04045:
@@ -70,10 +80,143 @@ def oklabToRgb(oklabData):
 
 
 def oklabDistance(colorData1, colorData2):
-    a, b, c = colorData1['oklab']
-    x, y, z = colorData2['oklab']
+    a, b, c = colorData1
+    x, y, z = colorData2
 
     diffSum = (a-x)**2 + (b-y)**2 + (c-z)**2
 
 
     return diffSum
+
+
+def pickImage():
+    currentlySelectedImagePath = filedialog.askopenfilename(filetypes=[("Image files", "*.png;*.jpg;*.jpeg;*.bmp;*.gif")], initialdir=".")
+    if currentlySelectedImagePath:
+        currentImage = Image.open(currentlySelectedImagePath)
+
+
+
+        return currentImage, currentlySelectedImagePath
+
+    return None, None
+
+def getInputImage():
+    global inputImage
+    global inputImagePath
+    global imageNameLabelText
+
+    inputImage, inputImagePath = pickImage()
+    if inputImagePath is None:
+        imageNameLabelText.set("Currently selected image: none")
+    else:
+        imageNameLabelText.set(f"Currently selected image: {inputImagePath}")
+
+def setPaletteData():
+    global inputPalette
+    global paletteData
+
+    paletteData = []
+
+    for y in range(paletteData.shape[0]):
+        for x in range(paletteData.shape[1]):
+            r, g, b = paletteData[y, x]
+            palettePixel = r, g, b
+
+            oklabColor = rgbToOklab(palettePixel)
+
+            paletteData.append(
+                {
+                    "rgb": palettePixel,
+                    "oklab": oklabColor
+                }
+             )
+
+def getPaletteImage():
+    global inputPalette
+    global inputPalettePath
+    global paletteNameLabelText
+
+    inputPalette, inputPalettePath = pickImage()
+
+    if inputPalettePath is None:
+        paletteNameLabelText.set("Currently selected palette: none")
+    else:
+        paletteNameLabelText.set(f"Currently selected palette: {inputPalettePath}")
+        setPaletteData()
+
+
+
+def getClosestColorFromPalette(color):
+    global inputPalette
+    global inputPalettePath
+
+    paletteData = np.array(inputPalette)
+
+    closestColor = None
+    closestDist = None
+
+    for paletteColorData in paletteData:
+            colorOklab = rgbToOklab(color)
+
+            dist = oklabDistance(paletteColorData['oklab'], colorOklab)
+
+            if closestDist is None or dist < closestDist:
+                closestColor = paletteColorData['rgb']
+                closestDist = dist
+
+    return closestColor
+def replacePalette():
+    global inputImage
+    global inputImagePath
+
+
+    if inputImage is None or inputPalette is None:
+        tk.messagebox.showerror(message="At least one of the inputs wasn't provided")
+        return
+
+    originalData = np.array(inputImage)
+
+    for y in range(originalData.shape[0]):
+        for x in range(originalData.shape[1]):
+            r,g,b = originalData[y, x]
+            originalPixel = r, g, b
+
+            originalData[y, x] = getClosestColorFromPalette(originalPixel)
+
+    imageAfterChanges = Image.fromarray(originalData)
+    pathToOriginalImageDir = os.path.dirname(inputImagePath)
+    originalImageName = os.path.splitext(os.path.basename(inputImagePath))[0]
+
+    paletteImageName = os.path.splitext(os.path.basename(inputPalettePath))[0]
+
+    newImagePath = f"{pathToOriginalImageDir}/{originalImageName}_{paletteImageName}.png"
+
+    imageAfterChanges.save(newImagePath)
+
+    tk.messagebox.showinfo(message=f"Successfully saved new image to: {newImagePath}")
+
+
+root = tk.Tk()
+root.title("Color Palette Replacer")
+root.minsize(400, 30)
+
+browseImagesButton = tk.Button(root, text="Pick input image", command=getInputImage, pady=5)
+browseImagesButton.pack(pady=10)
+
+imageNameLabelText = tk.StringVar()
+imageNameLabelText.set("Currently selected image: none")
+imageNameLabel = tk.Label(root, textvariable=imageNameLabelText, pady=10)
+imageNameLabel.pack()
+
+browsePalettesButton = tk.Button(root, text="Pick input palette", command=getPaletteImage, pady=5)
+browsePalettesButton.pack(pady=10)
+
+paletteNameLabelText = tk.StringVar()
+paletteNameLabelText.set("Currently selected palette: none")
+paletteNameLabel = tk.Label(root, textvariable=paletteNameLabelText, pady=10)
+paletteNameLabel.pack()
+
+browseImagesButton = tk.Button(root, text="Replace palette", command=replacePalette, pady=5)
+browseImagesButton.pack(pady=10)
+
+root.mainloop()
